f8be20e7d77bc10a7f9e474a25f618222255a527
MrDemonid <MrDemonid@yandex.ru>
2025-06-01 23:21:08 +0300

    proceed lesson 4

--- advanced/lesson_04/main2.py
+++ advanced/lesson_04/main2.py
@@ -10,6 +10,49 @@ def _sum():
     array1 = request.form.getlist("a", type=int)
     array2 = request.form.getlist("b", type=int)
 
+    result = ",".join(str(a1 + a2) for (a1, a2) in zip(array1, array2))
+    return f"Array of sum is [{result}]"
+
+
+@app.route("/sum2", methods=["POST"])
+def _sum2():
+    form_data = request.get_data(as_text=True)
+    if form_data:
+        request_data = unquote_plus(form_data)
+        print(f"form_data = {form_data}")
+        print(f"request_data = {request_data}")
+
+        arrays = {}
+        for encoding_chunk in request_data.split("&"):
+            k, v = encoding_chunk.split("=")
+            print("  encode: " + k + ", " + v)
+            arrays[k] = [int(i) for i in v.split(",")]
+
+        result = ",".join(str(a1 + a2) for (a1, a2) in zip(arrays["a"], arrays["b"]))
+        return f"Array of sum is [{result}]"
+
+    return "Ok", 200
+
+
+@app.route("/sum/json", methods=["POST"])
+def _sum_json():
+    data = request.get_json()
+
+    array1 = data.get("a", [])      # данные из ключа "a", или []
+    array2 = data.get("b", [])      # данные из ключа "b", или []
+
+    result = ",".join(str(a1+a2) for (a1, a2) in zip(array1, array2))
+    return f"Array of sum is [{result}]"
+
+
+@app.route("/sum/json2", methods=["POST"])
+def _sum_json2():
+    form_data = request.get_data()
+    data = json.loads(form_data)
+
+    array1 = data.get("a", [])      # данные из ключа "a", или []
+    array2 = data.get("b", [])      # данные из ключа "b", или []
+
     result = ",".join(str(a1+a2) for (a1, a2) in zip(array1, array2))
     return f"Array of sum is [{result}]"
 
